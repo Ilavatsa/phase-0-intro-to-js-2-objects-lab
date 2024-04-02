728351fb3135ce9545825d0342c9ae8a92a1e6b8
Felicity <felicitymuhonja33@gmail.com>
2024-04-02 11:42:43 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+  name: "Felicity",
+  streetAdress: "Nairobi",
+};
+function updateEmployeeWithKeyAndValue(object, key, value) {
+  let newObj = { ...object };
+  newObj[key] = value;
+  return newObj;
+}
+updateEmployeeWithKeyAndValue(employee, "Heskey", "Kilimani");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+function deleteFromEmployeeByKey(object, key) {
+  let newObj = { ...object };
+  delete newObj[key];
+  return newObj;
+}
+deleteFromEmployeeByKey(employee, streetAdress);
+
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+  delete object[key];
+  return object;
+}
